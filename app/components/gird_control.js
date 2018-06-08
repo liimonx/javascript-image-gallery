a040d9edca50f8db97d54985bc633ae6e89eb804
liimonx <limonkhan669@gmail.com>
2018-06-09 04:44:55 +0600

    added condition

--- app/components/gird_control.js
+++ app/components/gird_control.js
@@ -3,6 +3,11 @@ const grid2 = document.getElementById('grid_2')
 const grid3 = document.getElementById('grid_3')
 const grid4 = document.getElementById('grid_4')
 
+const grid1Img = document.querySelectorAll('#grid_1 img')
+const grid2Img = document.querySelectorAll('#grid_2 img')
+const grid3Img = document.querySelectorAll('#grid_3 img')
+const grid4Img = document.querySelectorAll('#grid_4 img')
+
 let a = grid1.childNodes.length
 let b = grid2.childNodes.length
 let c = grid3.childNodes.length
@@ -18,18 +23,28 @@ const gridControl = (a,b,c,d) => {
     arr.forEach( s => {
         if (s > 0) {
             if (s == a) {
-                console.log(grid1.children);
                 
             }else if (s == b){
-                console.log(grid2.children);
                 
             }else if (s == c){
-                console.log(grid3.children);
+
             }else if (s == d){
-                console.log(grid4.children);
+                
+            }
+        }else{
+            if (s == a) {
+                
+            }else if (s == b){
+                
+            }else if (s == c){
+
+            }else if (s == d){
+                
             }
         }
-    })    
+    })
+
+
        
 }
 
